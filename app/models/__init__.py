9628bfb165731767e6cd035fd2b765405ae10c25
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-26 02:34:40 +0530

    Register repair audit model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.customer import Customer
 from app.models.repair import RepairOrder
+from app.models.repair_audit import RepairAuditLog
 
-__all__ = ['User', 'Customer', 'RepairOrder']
+__all__ = ["User", "Customer", "RepairOrder", "RepairAuditLog"]
