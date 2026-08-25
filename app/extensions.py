58a3383bf52319e6db711075d78c35f83583aea8
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-26 02:23:06 +0530

    Add rate limiter extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,12 @@
-# app/extensions.py
-from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["300 per hour", "3000 per day"],
+)
