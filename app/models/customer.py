75b38ebe922a335ebd129d9c30ee07dab4495468
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-26 02:25:34 +0530

    Add customer indexes

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,9 +1,9 @@
 from app.extensions import db
 
+
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=True)
-    phone = db.Column(db.String(20), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    
+    name = db.Column(db.String(100), nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True, nullable=True, index=True)
+    phone = db.Column(db.String(20), nullable=False, index=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now(), index=True)
