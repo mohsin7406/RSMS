e94ea469a36b0e900851c40f091567e8e3a72548
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-26 15:13:33 +0530

    Disable CSRF in integration test app

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from app.extensions import db
 @pytest.fixture()
 def app():
     app = create_app("testing")
-    app.config.update(TESTING=True, WTF_CSRF_ENABLED=True)
+    app.config.update(TESTING=True, WTF_CSRF_ENABLED=False)
     with app.app_context():
         db.create_all()
     yield app
