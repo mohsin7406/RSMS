6cd266745805b3a2d9513a5eafee5f4908d74d7c
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-26 20:28:48 +0530

    Export job purchase model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -18,9 +18,11 @@ from app.models.notification_template import NotificationTemplate
 from app.models.sms_log import SMSLog
 from app.models.role_permission import RolePermission
 from app.models.repair_extra_charge import RepairExtraCharge
+from app.models.job_purchase import JobPurchase
 
 __all__ = [
     "User", "Customer", "RepairOrder", "RepairAuditLog", "Part", "PartUsage",
     "StockMovement", "Invoice", "Payment", "RepairQC", "WarrantyClaim", "Lead", "LeadContact", "Booking",
     "ServiceConfirmation", "RepairPhoto", "NotificationTemplate", "SMSLog", "RolePermission", "RepairExtraCharge",
+    "JobPurchase",
 ]
