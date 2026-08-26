f19e565427c2ace055a3017185cd3c1f2bbe72e0
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-26 17:13:10 +0530

    Register notification template model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -13,9 +13,10 @@ from app.models.lead import Lead
 from app.models.booking import Booking
 from app.models.service_confirmation import ServiceConfirmation
 from app.models.repair_photo import RepairPhoto
+from app.models.notification_template import NotificationTemplate
 
 __all__ = [
     "User", "Customer", "RepairOrder", "RepairAuditLog", "Part", "PartUsage",
     "StockMovement", "Invoice", "Payment", "RepairQC", "WarrantyClaim", "Lead", "Booking",
-    "ServiceConfirmation", "RepairPhoto",
+    "ServiceConfirmation", "RepairPhoto", "NotificationTemplate",
 ]
