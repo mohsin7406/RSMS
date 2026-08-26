3cd83c5344080a07e2f733ba0492ec07a3f98111
mohsin7406 <93346349+mohsin7406@users.noreply.github.com>
2026-08-27 02:00:49 +0530

    Export system update history model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -30,5 +30,6 @@ from app.models.operations import SupplierPayment, Expense, StockAllocation, Aud
 from app.models.system_setting import SystemSetting
 from app.models.qc_checklist_item import QCChecklistItem
 from app.models.setting_option import SettingOption
+from app.models.system_update import SystemUpdate
 
-__all__=["User","Customer","RepairOrder","RepairAuditLog","Part","PartUsage","PartUsageReturn","StockMovement","Invoice","Payment","RepairQC","WarrantyClaim","Lead","LeadContact","Booking","ServiceConfirmation","RepairPhoto","NotificationTemplate","SMSLog","RolePermission","RepairExtraCharge","JobPurchase","JobPurchaseReturn","InventoryCategory","Supplier","Purchase","PurchaseItem","PurchaseReturn","SupplierPayment","Expense","StockAllocation","AuditEvent","SystemSetting","QCChecklistItem","SettingOption"]
+__all__=["User","Customer","RepairOrder","RepairAuditLog","Part","PartUsage","PartUsageReturn","StockMovement","Invoice","Payment","RepairQC","WarrantyClaim","Lead","LeadContact","Booking","ServiceConfirmation","RepairPhoto","NotificationTemplate","SMSLog","RolePermission","RepairExtraCharge","JobPurchase","JobPurchaseReturn","InventoryCategory","Supplier","Purchase","PurchaseItem","PurchaseReturn","SupplierPayment","Expense","StockAllocation","AuditEvent","SystemSetting","QCChecklistItem","SettingOption","SystemUpdate"]
